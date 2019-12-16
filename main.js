970c6e46e5128786fca19fbd0a747a775eeae65e
LaceyMae <laceymaecreative@gmail.com>
2019-12-16 15:29:07 -0600

    added Toddler_DDR.css

---
+++ main.js
@@ -0,0 +1,18 @@
+// let leftArrow = document.createElement('h1');
+// let my_span = document.createElement('span');
+
+
+// my_span.innerText = "Left Arrow";
+
+// leftArrow.appendChild(my_span);
+
+// leftArrow.classList.add('custom_class');
+
+// document.body.appendChild(leftArrow);
+
+
+// let leftArrow = document.getElementByClass('arrow');
+// var arrow = random;
+
+
+
